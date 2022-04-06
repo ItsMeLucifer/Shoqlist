9c31aefbe92621729e8275176485a32d2695318e
ItsMeLucifer <lucifer@int.pl>
2022-04-06 19:05:24 +0200

    Fixed bug related to not correctly persisting user's credentials after closing the app

--- lib/widgets/wrapper.dart
+++ lib/widgets/wrapper.dart
@@ -1,22 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shoqlist/main.dart';
 import 'package:shoqlist/pages/authentication.dart';
 import 'package:shoqlist/pages/home_screen.dart';
-import 'package:shoqlist/viewmodels/firebase_auth_view_model.dart';
 
 class Wrapper extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final firebaseAuthVM = ref.watch(firebaseAuthProvider);
+    final _auth = FirebaseAuth.instance;
     firebaseAuthVM.addListenerToFirebaseAuth();
-    switch (firebaseAuthVM.status) {
-      case Status.Authenticated:
-        return HomeScreen(ref);
-        break;
-      default:
+    return StreamBuilder<User>(
+      stream: _auth.authStateChanges(),
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.hasData && (!snapshot.data.isAnonymous)) {
+          return HomeScreen(ref);
+        }
         return Authentication();
-        break;
-    }
+      },
+    );
   }
 }
